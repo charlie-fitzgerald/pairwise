2f6f702378f044799fdda1bd41c7c4ec461d74e4
charlie-fitzgerald <46912450+charlie-fitzgerald@users.noreply.github.com>
2025-01-26 19:59:08 -0800

    Following Along

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import Message from './components/Message';
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Francisco", "London", "Tokyo", "Paris"];
+
+  const handleSelectItem = (item) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <Message />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
-  )
+  );
 }
 
 export default App;
